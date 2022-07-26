ef5290f222e0bfc255fa02d7254fb707a9888695
Ankit080197 <109266755+Ankit080197@users.noreply.github.com>
2022-07-27 01:26:53 +0530

    hello world

---
+++ 18-07-22/hello.java
@@ -0,0 +1,12 @@
+/* write a program to print 
+hello world
+@Ankit Sharma*/
+
+class hello     //main class
+{
+public static void main (String args[])     //main method
+{
+
+System.out.println("hello world");       //print output
+}
+}
