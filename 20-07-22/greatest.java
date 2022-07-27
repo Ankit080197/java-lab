7e4bc335c0f3384ad02f40bd683cbf17ee69ee6e
Ankit080197 <109266755+Ankit080197@users.noreply.github.com>
2022-07-27 20:36:33 +0530

    greatest

---
+++ 20-07-22/greatest.java
@@ -0,0 +1,19 @@
+/*write a progrm to find greatest 
+between two numbers
+@Ankit Sharma*/
+
+class greatestNumber
+{
+  public static void main(String args[])    //main method
+ {
+         int x=3423,y=4534;          //declaring variables
+    if (x>y)
+  {
+         System.out.println(x+"is greatest than "+y);    //print if x greater
+  }
+    else
+  {
+         System.out.println(y+"is greatest than "+x);     //print if y greater
+  }
+ }
+}
